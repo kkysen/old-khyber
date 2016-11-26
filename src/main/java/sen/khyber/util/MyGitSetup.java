e40744cfc9d093bbf0b1bcf515b2030b6e8e0d53
Khyber Sen <kkysen@gmail.com>
2016-11-26 01:26:35 -0500

    wrote MyGitSetup class for setting up git on school computer

--- src/main/java/sen/khyber/util/MyGitSetup.java
+++ src/main/java/sen/khyber/util/MyGitSetup.java
@@ -1,6 +1,8 @@
 package sen.khyber.util;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class MyGitSetup {
     
@@ -10,20 +12,63 @@ public class MyGitSetup {
         runtime = Runtime.getRuntime();
     }
     
+    private void rawExec(final String command) throws IOException {
+        runtime.exec(command);
+        //System.out.println(command);
+    }
+    
     public void exec(final String command) throws IOException {
-        runtime.exec("git " + command);
+        rawExec("git " + command);
     }
     
     public void config(final String property, final String value) throws IOException {
         exec("config --global " + property + " " + value);
     }
     
+    public void config() throws IOException {
+        config("user.name", "Khyber Sen");
+        config("user.email", "kkysen@gmail.com");
+        config("push.default", "simple");
+        config("core.editor", "emacs");
+    }
+    
+    public void add(final String arg) throws IOException {
+        exec("add " + arg);
+    }
+    
+    public void commit(final String message) throws IOException {
+        exec("commit -m " + '"' + message + '"');
+    }
+    
+    public void push(final String message) throws IOException {
+        add("-A");
+        commit(message);
+        exec("push");
+    }
+    
+    public void makeFile(final String fileName, final String contents) throws IOException {
+        rawExec("echo \"" + contents.replace("\"", "\\\"") + "\" > " + fileName);
+    }
+    
+    public String generateSshKeys() throws IOException {
+        rawExec("ssh-keygen -t rsa -b 4096 -C kkysen@gmail.com");
+        rawExec(""); // specify no password
+        return new String(Files.readAllBytes(Paths.get("~/.ssh/id_rsa.pub")));
+    }
+    
+    public void clone(final String url) throws IOException {
+        exec("clone " + url);
+    }
+    
     public static void main(final String[] args) throws Exception {
         final MyGitSetup git = new MyGitSetup();
-        git.config("user.name", "Khyber Sen");
-        git.config("user.email", "kkysen@gmail.com");
-        git.config("push.default", "simple");
-        git.config("core.editor", "emacs");
+        final String rsaPubKey = git.generateSshKeys();
+        System.out.println(rsaPubKey);
+        System.in.read();
+        git.clone("git@github.com:kkysen/MKS21X.git");
+        git.config();
+        git.makeFile("school.txt", "testing from school...");
+        git.push("tested from school computer");
     }
     
 }
