51baa5c99107390dacd6e91ababbafc637a97a35
Khyber Sen <kkysen@gmail.com>
2016-12-19 11:59:42 -0500

    Update RuntimeAnnotations.java

--- src/main/java/sen/khyber/util/RuntimeAnnotations.java
+++ src/main/java/sen/khyber/util/RuntimeAnnotations.java
@@ -1,1 +1,13 @@
 public class RuntimeAnnotations {
+    
+    public static void processMethods(Class<?> type, Consumer<Method> methodProcessor) {
+        for (Method method : type.getDeclaredMethods()) {
+            methodProcessor.accept(method);
+        }
+    }
+    
+    public static void processMethodsWithAnnotation(Class<?> type, Class<? extends Annotation> annotation, Consumer<Method> methodProcessor) {
+        processMethods(type, method -> {if (method.isAnnotationPresent(annotation)) methodProcessor.accept(method);});
+    }
+    
+}
