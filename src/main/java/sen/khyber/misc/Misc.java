62cb75bee77df07c2a70c803aeb5e4edea94bdc7
Khyber Sen <kkysen@gmail.com>
2017-02-01 21:01:52 -0500

    a bunch of old stuff

--- src/main/java/sen/khyber/misc/Misc.java
+++ src/main/java/sen/khyber/misc/Misc.java
@@ -1,16 +1,9 @@
 package sen.khyber.misc;
 
-import sen.khyber.stats.counter.WordCounter;
-import sen.khyber.web.Internet;
-
-import java.nio.file.Paths;
-
 public class Misc {
     
     public static void main(final String[] args) throws Exception {
-        final WordCounter words = WordCounter.fromCSV(Paths.get("C:/Users/kkyse/OneDrive/CS/Eclipse/git/Khyber/gutenbergEnglishWordCounts.csv"));
-        System.out.println(words.size());
-        System.out.println(words.length());
+        
     }
     
 }
